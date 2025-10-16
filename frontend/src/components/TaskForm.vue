3be10c34e79f7c5f2d40d67d41b729bdca8717a6
yzkoh2 <yongzhao249@gmail.com>
2025-10-16 17:39:37 +0800

    fixed rabbitmq and task recurrance date

--- frontend/src/components/TaskForm.vue
+++ frontend/src/components/TaskForm.vue
@@ -64,7 +64,7 @@
         </div>
         <div v-if="localData.recurrence_interval === 'custom'">
           <label class="block text-sm font-medium text-gray-700 mb-2">Recurrence Days</label>
-          <input v-model="localData.recurrence_days" type="number"
+          <input v-model="localData.recurrence_days" type="number" min="1" required
             class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent">
         </div>
         <div>
